a75885b59bde56d26e1154aeadf99a5f23677238
Yukako Shimada <kks2.ohs38@gmail.com>
2021-06-10 16:43:08 +0900

    heroku

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.views.generic.base import View
+from django.http.response import HttpResponse
+
+class CallbackView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('OK')
